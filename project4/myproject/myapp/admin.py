838d27e63d154093a94f1a2a661c756b023bee85
Navya718 <anavyasree500@gmail.com>
2025-07-11 14:54:14 +0530

    Add files via upload

---
+++ project4/myproject/myapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import EmployeeModel
+from .models import FacultyModel
+from .models import StudentModel
+admin.site.register(EmployeeModel)
+admin.site.register(FacultyModel)
+admin.site.register(StudentModel)
+
+
+
+# Register your models here.
